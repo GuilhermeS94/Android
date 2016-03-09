2d905e91ce31b2835bf410d1d406c83b7550f6de
Guilherme Silva <guilherme.frr.silva@outlook.com>
2016-03-09 14:43:41 -0300

    Alterando labels da view antes de serem exibidos.

--- Exercise/src/br/com/guilhermes/exerciseNu/NoticeActivity.java
+++ Exercise/src/br/com/guilhermes/exerciseNu/NoticeActivity.java
@@ -1,9 +1,10 @@
 package br.com.guilhermes.exerciseNu;
 
+import java.util.Date;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 public class NoticeActivity extends Activity {
 
@@ -11,5 +12,18 @@ public class NoticeActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_notice);
+		
+		//Solicitar o get
+		AlterarTitulo();
+	}
+	
+	/*Tratar a solicitação Get
+	 * e atualizar a View com
+	 * o JSON retornado
+	 **/
+	private void AlterarTitulo()
+	{
+		TextView nvTitulo = (TextView) findViewById(R.id.lblTitulo);			        
+        nvTitulo.setText(new Date().toString());
 	}
 }
