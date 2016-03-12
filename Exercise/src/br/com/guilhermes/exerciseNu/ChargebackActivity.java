8748072b8d0981ada081eaddb2a66ac28752b8b7
Guilherme Silva <guilherme.frr.silva@outlook.com>
2016-03-12 16:06:07 -0300

    Conclusão da primeira tela e eventos click, inicio da segunda tela, apenas switches presentes.

---
+++ Exercise/src/br/com/guilhermes/exerciseNu/ChargebackActivity.java
@@ -0,0 +1,15 @@
+package br.com.guilhermes.exerciseNu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChargebackActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_chargeback);
+		
+		
+	}
+}
--- Exercise/src/br/com/guilhermes/exerciseNu/NoticeActivity.java
+++ Exercise/src/br/com/guilhermes/exerciseNu/NoticeActivity.java
@@ -1,11 +1,15 @@
 package br.com.guilhermes.exerciseNu;
 
+import java.util.Iterator;
 import java.util.concurrent.ExecutionException;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
@@ -14,6 +18,7 @@ import android.widget.TextView;
 
 public class NoticeActivity extends Activity {
 
+	private JSONObject json;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,9 +27,9 @@ public class NoticeActivity extends Activity {
 		//Solicitar o get
 		try {
 			String resp = new TratarRequests().execute(StaticVars.NOTICE).get();
-			JSONObject json = new TratarJson().ConverterJsonParaJSONObject(resp);
-			TextView nvTitulo = (TextView) findViewById(R.id.lblTitulo);
-			//nvTitulo.setTypeface(null, Typeface.BOLD);
+			json = new TratarJson().ConverterJsonParaJSONObject(resp);
+	        
+			TextView nvTitulo = (TextView) findViewById(R.id.lblTitulo);			
 			TextView nvConteudo = (TextView) findViewById(R.id.lblConteudo);
 			nvTitulo.setText(json.get(StaticVars.TITLE).toString());
 			nvConteudo.setText(Html.fromHtml(json.get(StaticVars.DESCRIPTION).toString()));
@@ -41,6 +46,28 @@ public class NoticeActivity extends Activity {
             public void onClick(View v) {
                 finish();
             	System.exit(0);
+            }
+        });
+        
+        Button btnCont = (Button) findViewById(R.id.btnContinuar);
+        btnCont.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	Intent chargeBack = new Intent(NoticeActivity.this, ChargebackActivity.class);
+            	String resp;
+				try {
+					JSONObject links = json.getJSONObject(StaticVars.LINKS);
+					JSONObject chgBack = links.getJSONObject(StaticVars.CHARGEBACK);
+					resp = new TratarRequests().execute(chgBack.get(StaticVars.HREF).toString()).get();
+					chargeBack.putExtra("conteudo", resp);
+	            	startActivity(chargeBack);
+	            	
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}                
             }
         });
 	}	
